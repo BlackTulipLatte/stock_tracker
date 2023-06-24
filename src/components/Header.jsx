0099a2b2a6f3b19ffc16104bc75217f1a7d269bf
Wonjae Cho <51027177+jadencho1014@users.noreply.github.com>
2023-06-24 17:00:19 -0400

    dark mode added, scrollbar styling fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React from "react";
 import Search from "./Search";
-import Themeicon from './Themeicon';
-import Login from './Login';
+import Themeicon from "./Themeicon";
+import Login from "./Login";
 
 const Header = ({ name }) => {
-  return <>
-    <div className="x1:px-32">
-      <h1 className="text-5xl">{name}</h1>
-      <Search />
-    </div>
-    <div><Themeicon /></div>
-    <div><Login /></div>
-
-  </>;
+  return (
+    <>
+      <div className="x1:px-32">
+        <h1 className="text-5xl">{name}</h1>
+        <Search />
+      </div>
+      <div>
+        <Themeicon />
+      </div>
+      <div>
+        <Login />
+      </div>
+    </>
+  );
 };
 
 export default Header;
--- src/components/Themeicon.jsx
+++ src/components/Themeicon.jsx
@@ -1,28 +1,30 @@
-import React, { useContext } from 'react';
-import { MoonIcon } from '@heroicons/react/solid';
-import ThemeContext from '../context/ThemeContext';
+import React, { useContext } from "react";
+import { MoonIcon } from "@heroicons/react/solid";
+import ThemeContext from "../context/ThemeContext";
 const Themeicon = () => {
-  const {darkMode, setDarkMode} = useContext(ThemeContext);
-  
+  const { darkMode, setDarkMode } = useContext(ThemeContext);
+
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
-  }
+  };
 
-  
   return (
-  <button className={`rounded-lg border-1 border-neutral-400 p-2 
+    <button
+      className={`rounded-lg border-1 border-neutral-400 p-2 
                       absolute right-24 xl:right-32 shadow-lg 
-                      ${darkMode ? "shadow-gray-100" : null}`}
-          onClick={toggleDarkMode}
-          >
-    <MoonIcon className={`h-8 w-8 cursor-pointer stroke-1 fill-none stroke-neutral-400
-                          ${darkMode 
-                            ? "fill-yellow-500 stroke-yellow-400" 
-                            : "fill-none stroke-neutral-400"
-                          }`} 
-                            />
-  </button>
+                      ${darkMode ? "shadow-gray-800" : null}`}
+      onClick={toggleDarkMode}
+    >
+      <MoonIcon
+        className={`h-8 w-8 cursor-pointer stroke-1 fill-none stroke-neutral-400
+                          ${
+                            darkMode
+                              ? "fill-yellow-500 stroke-yellow-400"
+                              : "fill-none stroke-neutral-400"
+                          }`}
+      />
+    </button>
   );
-}
+};
 
-export default Themeicon
+export default Themeicon;
