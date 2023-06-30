2ce341a66c43cf3d5555ce7aa59f8e65ff70a63c
BlackTulipLatte <86974706+BlackTulipLatte@users.noreply.github.com>
2023-06-30 11:45:49 -0400

    added api tests

--- src/tests/api.test.js
+++ src/tests/api.test.js
@@ -1,29 +1,37 @@
-import { searchSymbol } from '../util/API';
+import { searchStock, searchQuote, getHistoricalData } from '../util/API';
 import fetch from 'jest-fetch-mock';
 
 describe('API Tests', () => {
-  beforeEach(() => {
-    fetch.resetMocks();
-  });
-
-  it('should search symbol', async () => {
+  it('symbol search', async () => {
     const query = 'AAPL';
-    const expectedResponse = { symbol: 'AAPL', name: 'Apple Inc.' };
+    const expectedResponse = {"country":"US","currency":"USD","estimateCurrency":"USD","exchange":"NASDAQ NMS - GLOBAL MARKET","finnhubIndustry":"Technology","ipo":"1980-12-12","logo":"https://static2.finnhub.io/file/publicdatany/finnhubimage/stock_logo/AAPL.svg","marketCapitalization":3025129.5374395857,"name":"Apple Inc","phone":"14089961010.0","shareOutstanding":15728.7,"ticker":"AAPL","weburl":"https://www.apple.com/"};
     fetch.mockResponseOnce(JSON.stringify(expectedResponse));
 
-    const result = await searchSymbol(query);
-
-    expect(fetch).toHaveBeenCalledWith(
-      `https://finnhub.io/api/v1/search?q=${query}&token=ci8ct81r01qnrgm35310ci8ct81r01qnrgm3531g`
-    );
+    const result = await searchStock(query);
     expect(result).toEqual(expectedResponse);
   });
 
-  it('should throw an error when response is not OK', async () => {
+  it('quote search', async () => {
+    // NOTE: THIS TEST MAY FAIL SINCE THE PRICE OF THE STOCK IS VOLATILE. AS LONG AS THE API CALL IS SUCCESSFUL, THE TEST IS CONSIDERED A SUCCESS
     const query = 'AAPL';
-    const errorMessage = 'An error has occurred: 404';
-    fetch.mockResponseOnce(JSON.stringify({}), { status: 404 });
+    const expectedResponse = {"c":192.36,"d":2.77,"dp":1.461,"h":193.15,"l":191.265,"o":191.63,"pc":189.59,"t":1688139451};
+    fetch.mockResponseOnce(JSON.stringify(expectedResponse));
 
-    await expect(searchSymbol(query)).rejects.toThrow(errorMessage);
+    const result = await searchQuote(query);
+    expect(result).toEqual(expectedResponse);
   });
+
+  it('historical data search', async () => {
+    // NOTE: THIS TEST MAY FAIL SINCE THE PRICE OF THE STOCK IS VOLATILE. AS LONG AS THE API CALL IS SUCCESSFUL, THE TEST IS CONSIDERED A SUCCESS
+    const query = 'AAPL';
+    const expectedResponse = {"c":192.36,"d":2.77,"dp":1.461,"h":193.15,"l":191.265,"o":191.63,"pc":189.59,"t":1688139451};
+    fetch.mockResponseOnce(JSON.stringify(expectedResponse));
+
+    const result = await getHistoricalData(query, 1, '2020-01-01', '2020-01-02');
+    expect(result).toEqual(expectedResponse);
+  });
+
+
+
+
 });
